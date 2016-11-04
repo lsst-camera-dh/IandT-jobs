9c749e2410a7b528d519de669c1356f3bc0cec1e
Eric Charles <badass@stanford.edu>
2016-11-04 16:06:53 -0700

    Turn off dry_run=True in producer_fe55_raft_acq_sim.py

--- harnessed_jobs/fe55_raft_acq_sim/v0/producer_fe55_raft_acq_sim.py
+++ harnessed_jobs/fe55_raft_acq_sim/v0/producer_fe55_raft_acq_sim.py
@@ -14,4 +14,4 @@ RAFT = fake_raft.Raft.create_from_etrav(fake_raft.RAFT_ID)
 for image_type in IMGTYPES:
     RAFT.file_copy(PROCESS_NAME_IN, fake_raft.OUTPATH,
                    test_type=TESTTYPE, image_type=image_type,
-                   pattern=PATTERN, sort=True, dry_run=True)
+                   pattern=PATTERN, sort=True)
