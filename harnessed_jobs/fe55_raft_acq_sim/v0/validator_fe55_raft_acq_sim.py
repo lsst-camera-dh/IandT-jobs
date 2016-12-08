1c0d883ac3b8016d5fd212e7d39f5f88a892631c
Eric Charles <badass@stanford.edu>
2016-12-08 15:02:45 -0800

    modify harnessed sim jobs to call siteUtils, rather than using value for fakeRaft.py

--- harnessed_jobs/fe55_raft_acq_sim/v0/validator_fe55_raft_acq_sim.py
+++ harnessed_jobs/fe55_raft_acq_sim/v0/validator_fe55_raft_acq_sim.py
@@ -3,12 +3,15 @@
 
 import glob
 import lcatr.schema
+import siteUtils
 from simulation import fake_raft
 
 PATTERN = '*_fe55*.fits'
+OUTPATH = '.'
+JOB_ID = siteUtils.getJobName()
 
-OUTREGEXP = fake_raft.make_outfile_path(outpath=fake_raft.OUTPATH, slot_name="*",
-                                        file_string=PATTERN, job_id=fake_raft.JOB_ID)
+OUTREGEXP = fake_raft.make_outfile_path(outpath=OUTPATH, slot_name="*",
+                                        file_string=PATTERN, job_id=JOB_ID)
 
 RESULTS = []
 FILES = sorted(glob.glob(OUTREGEXP))
--- harnessed_jobs/flat_pair_raft_acq_sim/v0/producer_flat_pair_raft_acq_sim.py
+++ harnessed_jobs/flat_pair_raft_acq_sim/v0/producer_flat_pair_raft_acq_sim.py
@@ -2,16 +2,19 @@
 """ Producer script """
 
 from simulation import fake_raft
+import siteUtils
 
 # These are specific to this test
 TESTTYPE = 'FLAT'
 IMGTYPES = ['FLAT']
 PROCESS_NAME_IN = 'vendorIngest'
 PATTERN = '*_flat*.fits'
+OUTPATH = '.'
+RAFT_ID = siteUtils.getUnitId()
 
-RAFT = fake_raft.Raft.create_from_etrav(fake_raft.RAFT_ID)
+RAFT = fake_raft.Raft.create_from_etrav(RAFT_ID)
 
 for image_type in IMGTYPES:
-    RAFT.file_copy(PROCESS_NAME_IN, fake_raft.OUTPATH,
+    RAFT.file_copy(PROCESS_NAME_IN, OUTPATH,
                    test_type=TESTTYPE, image_type=image_type,
                    pattern=PATTERN, sort=True)
