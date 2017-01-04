6b9f606ccc07a4c56b70e78eb97c78f83da9df6d
Jim Chiang <jchiang@slac.stanford.edu>
2017-01-03 17:24:15 -0800

    add read noise harnessed job

--- harnessed_jobs/read_noise_raft/v0/producer_read_noise_raft.py
+++ harnessed_jobs/read_noise_raft/v0/producer_read_noise_raft.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python
+from __future__ import print_function
+import sys
+import lsst.eotest.sensor as sensorTest
+import siteUtils
+import eotestUtils
+import simulation.fake_raft
+
+siteUtils.aggregate_job_ids()
+raft_id = siteUtils.getUnitId()
+db_name = 'Dev'
+
+raft = simulation.fake_raft.Raft.create_from_etrav(raft_id, db_name=db_name)
+
+for sensor_id in raft.sensor_names:
+    sensor_id = str(sensor_id)
+    #
+    # Use Fe55 exposures and the overscan region instead of the bias
+    # frames as per 2015-09-10 TS1-TS3 sprint decision:
+    # https://confluence.slac.stanford.edu/display/LSSTCAM/Science+Raft+Teststands
+    #
+    bias_files = siteUtils.dependency_glob('S*/%s_fe55_fe55_*.fits' % sensor_id,
+                                           jobname='fe55_raft_acq_sim',
+                                           description='Fe55 files for read noise:')
+    bias_files = [str(x) for x in bias_files]
+    gains = eotestUtils.getSensorGains(jobname='fe55_raft_analysis',
+                                       sensor_id=sensor_id)
+    system_noise = eotestUtils.getSystemNoise(gains)
+    if system_noise is None:
+        print()
+        print("WARNING: The system noise file is not given in")
+        print("config/%s/eotest_calibrations.cfg." % siteUtils.getSiteName())
+        print("The system noise will be set to zero for all amplifiers.")
+        print()
+        sys.stdout.flush()
+
+    mask_files = \
+        eotestUtils.glob_mask_files(pattern='%s_*mask.fits' % sensor_id)
+
+    task = sensorTest.ReadNoiseTask()
+    task.run(sensor_id, bias_files, gains, system_noise=system_noise,
+             mask_files=mask_files, use_overscan=True)
--- harnessed_jobs/read_noise_raft/v0/validator_read_noise_raft.py
+++ harnessed_jobs/read_noise_raft/v0/validator_read_noise_raft.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python
+from __future__ import print_function
+import glob
+import lsst.eotest.sensor as sensorTest
 import lcatr.schema
+import siteUtils
+import eotestUtils
+import simulation.fake_raft
+
+raft_id = siteUtils.getUnitId()
+db_name = 'Dev'
+
+raft = simulation.fake_raft.Raft.create_from_etrav(raft_id, db_name=db_name)
+slots = dict((str(x[1]), str(x[0])) for x in raft.items())
 
 results = []
+for sensor_id in raft.sensor_names:
+    ccd_vendor = sensor_id.split('-')[0]
+    read_noise_file = '%s_eotest_results.fits' % sensor_id
+    data = sensorTest.EOTestResults(read_noise_file)
+    amps = data['AMP']
+    read_noise_data = data['READ_NOISE']
+    system_noise_data = data['SYSTEM_NOISE']
+    total_noise_data = data['TOTAL_NOISE']
+    for amp, read_noise, system_noise, total_noise in zip(amps, read_noise_data,
+                                                          system_noise_data,
+                                                          total_noise_data):
+        results.append(lcatr.schema.valid(lcatr.schema.get('read_noise_raft'),
+                                          amp=amp, read_noise=read_noise,
+                                          system_noise=system_noise,
+                                          total_noise=total_noise,
+                                          slot=slots[sensor_id],
+                                          sensor_id=sensor_id))
+
+    results.extend(siteUtils.jobInfo())
+
+    results.append(eotestUtils.eotestCalibrations())
+
+    fe55_acq_job_id = siteUtils.get_prerequisite_job_id('S*/%s_fe55_fe55_*.fits' % sensor_id,
+                                                        jobname='fe55_raft_acq_sim')
+    md = dict(system_noise_file=dict(JOB_ID=fe55_acq_job_id))
+    results.extend(eotestUtils.eotestCalibsPersist('system_noise_file',
+                                                   metadata=md))
+
+    files = glob.glob('%s_read_noise?*.fits' % sensor_id)
+    for fitsfile in files:
+        eotestUtils.addHeaderData(fitsfile, LSST_NUM=sensor_id, TESTTYPE='FE55',
+                                  DATE=eotestUtils.utc_now_isoformat(),
+                                  CCD_MANU=ccd_vendor.upper())
+
+    data_products = [lcatr.schema.fileref.make(item) for item in files]
+    results.extend(data_products)
 
 lcatr.schema.write_file(results)
 lcatr.schema.validate_file()
