14f4099ff813bffa21f70463b0a254d4db6da20c
Jim Chiang <jchiang@slac.stanford.edu>
2017-03-23 16:06:12 -0700

    refactor channel name generation and REB ID/power line mapping

--- harnessed_jobs/rtm_aliveness_power_on/v0/ccs_rtm_aliveness_power_on.py
+++ harnessed_jobs/rtm_aliveness_power_on/v0/ccs_rtm_aliveness_power_on.py
@@ -13,7 +13,9 @@ from org.lsst.ccs.scripting import CCS
 
 CCS.setThrowExceptions(True)
 
-logging.basicConfig(format="%(message)s", level=logging.INFO,
+logging.basicConfig(format="%(message)s",
+#                    level=logging.DEBUG,
+                    level=logging.INFO,
                     stream=sys.stdout)
 logger = logging.getLogger()
 
@@ -24,27 +26,32 @@ class CcsSubsystems(object):
         for key, value in subsystems.items():
             self.__dict__[key] = CCS.attachSubsystem(value)
 
-ChannelInfo = namedtuple('ChannelInfo', ['pwr_chan', 'reb_chan', 'low_lim',
-                                         'high_lim', 'chkreb'])
+ChannelInfo = namedtuple('ChannelInfo', ['reb_ps_channel', 'ts8_mon_chan',
+                                         'low_lim', 'high_lim', 'chkreb'])
 
-channels = dict(digital=ChannelInfo('digital', 'DigI', 6., 800., False),
-                analog=ChannelInfo('analog', 'AnaI', 6., 610., False),
-                clockhi=ChannelInfo('clockhi', 'ClkHI', 6., 300., True),
-                clocklo=ChannelInfo('clocklo', 'ClkLI', 6., 300., True),
-                od=ChannelInfo('OD', 'ODI', 6., 190., True))
+channels = dict(digital=ChannelInfo('REB%d.digital.IaftLDO', 'R00.Reb%d.DigI',
+                                    6., 800., False),
+                analog=ChannelInfo('REB%d.analog.IaftLDO', 'R00.Reb%d.AnaI',
+                                   6., 610., False),
+                clockhi=ChannelInfo('REB%d.clockhi.IaftLDO', 'R00.Reb%d.ClkHI',
+                                    6., 300., True),
+                clocklo=ChannelInfo('REB%d.clocklo.IaftLDO', 'R00.Reb%d.ClkLI',
+                                    6., 300., True),
+                od=ChannelInfo('REB%d.OD.IaftLDO', 'R00.Reb%d.ODI',
+                               6., 190., True))
 
-def check_currents(ccs_sub, rebid, pwr_chan, reb_chan, low_lim, high_lim,
-                   chkreb, logger=logger):
+def check_values(ccs_sub, rebid, pwr, rebps_channel, ts8_mon_chan, low_lim,
+                 high_lim, chkreb, logger=logger):
     """
     Check that PS current levels are within specified range.
     """
-    reb_channel_name = "REB%d.%s.IaftLDO" % (rebid, pwr_chan)
+    reb_channel_name = rebps_channel % rebid
     command = "getChannelValue %s" % reb_channel_name
     logger.debug(command)
     cur_ps = ccs_sub.rebps.synchCommand(10, command).getResult()
     logger.info("REB PS: %s = %s", reb_channel_name, cur_ps)
 
-    ts8_channel_name = "R00.Reb%d.%s" % (rebid, reb_chan)
+    ts8_channel_name = ts8_mon_chan % rebid
     command = "getChannelValue %s" % ts8_channel_name
     logger.debug(command)
     cur_reb = ccs_sub.ts8.synchCommand(10, command).getResult()
@@ -62,13 +69,6 @@ def check_currents(ccs_sub, rebid, pwr_chan, reb_chan, low_lim, high_lim,
                            reb_channel_name, ts8_channel_name)
     logger.info("")
 
-try:
-    cdir = tsCWD
-    unit = CCDID
-except java.lang.Exception as eobj:
-    logger.debug(str(eobj))
-    logger.info("Running in standalone mode, outside of JH/eT.")
-
 ccs_sub = CcsSubsystems()
 
 logger.info("start tstamp: %f", time.time())
@@ -77,56 +77,44 @@ result = ccs_sub.rebps.synchCommand(10, "getChannelNames")
 channames = result.getResult()
 logger.debug(channames)
 
+# Map REB IDs and power lines.
+rebids = ccs_sub.ts8.synchCommand(10, "getREBIds").getResult()
 idmap = []
-# Get desired REBs from command line arguments
-# @todo: refactor this.
-for arg in sys.argv:
-    if ":" in arg:
-        idmap.append(arg)
-
-if len(idmap) == 0:
-    # Loop over all three REBs.
-    rebids = ccs_sub.ts8.synchCommand(10, "getREBIds").getResult()
-    for rebid in rebids:
-        logger.info("rebid = %s" % rebid)
-        idmap.append("%d:%d" % (int(rebid), int(rebid)))
+for item in rebids:
+    rebid = int(item)
+    # For now, just assume power line = REB ID.
+    pwrid = rebid
+    idmap.append((pwrid, rebid))
 
 print "Will attempt to power on:"
-for ids in idmap:
-    pwrid = int(ids.split(":")[0])
-    rebid = int(ids.split(":")[1])
+for pwrid, rebid in idmap:
     logger.info("power line %d for REB ID %d", pwrid, rebid)
 
-# @todo: determine if 0.1 sampling is needed
-#print "setting tick and monitoring period to 0.1s"
-#ccs_sub.ts8.synchCommand(10, "change tickMillis 100");
-#ccs_sub.ts8.synchCommand(10, "setTickMillis 100")
+print "Setting tick and monitoring period to 0.1s"
+ccs_sub.ts8.synchCommand(10, "change monitor-update taskPeriodMillis 100")
+#ccs_sub.rebps.synchCommand(10, "setUpdatePeriod 100")
 
-current_check_ok = True
-for ids in idmap:
-    pwrid = int(ids.split(":")[0])
-    rebid = int(ids.split(":")[1])
+# Ensure that power is off to all three REBs before proceeding with
+# the power-on sequences.
+for pwrid, rebid in idmap:
+    ccs_sub.rebps.synchCommand(20, "setNamedPowerOn %d master False" % pwrid);
+time.sleep(3)
 
+# This is the order to power on the various REB lines.
+power_on_list = ['master', 'digital', 'analog',
+                 'clockhi', 'clocklo', 'heater', 'od']
+
+power_on_ok = True
+for pwrid, rebid in idmap:
     rebname = 'REB%d' % rebid
     logger.info("*****************************************************")
     logger.info("Starting power-on procedure for %s (power line %s)",
                 rebname, pwrid)
     logger.info("*****************************************************")
 
-    # Verify that all power is off to start.
-    # @todo: check whether this should use pwrid or rebid
-    ccs_sub.rebps.synchCommand(20, "setNamedPowerOn %d master False" % pwrid);
-
-    time.sleep(3.0)
-
-    # Attempt to apply the REB power line by line:
-    powers = ['master', 'digital', 'analog', 'clockhi', 'clocklo',
-              'heater', 'od']
-
-    for pwr in powers:
+    for pwr in power_on_list:
         if 'clockhi' in pwr:
             pass
-# @todo: determine where reboot should really occur
 #            logger.info("Rebooting the RCE after a 5s wait")
 #            time.sleep(5.0)
 #            sout = subprocess.check_output("$HOME/rebootrce.sh", shell=True)
@@ -141,40 +129,40 @@ for ids in idmap:
             logger.info("%s: failed to turn on current %s!", rebname, pwr)
             raise eobj
 
-        time.sleep(2)
+        time.sleep(10)
         try:
             if pwr in channels:
-                check_currents(ccs_sub, rebid, *channels[pwr])
+                check_values(ccs_sub, rebid, pwr, *channels[pwr])
         except java.lang.Exception as eobj:
             logger.info("%s: current check failed for %s", rebname, pwr)
             logger.info(eobj.message)
-            current_check_ok = False
+            power_on_ok = False
             break
 
         time.sleep(2)
 
     logger.info("Proceed to turn on REB clock and rail voltages")
-#    # load default configuration
-# @todo: fix this
-#    ccs_sub.ts8.synchCommand(10, "loadCategories Rafts:itl")
-#    ccs_sub.ts8.synchCommand(10, "loadCategories RaftsLimits:itl")
-#    logger.info("loaded configurations: Rafts:itl")
+    # load default configuration
+    # @todo: Make sure etc folder has correct properities files for
+    # these configurations.
+    ccs_sub.ts8.synchCommand(10, "loadCategories Rafts:itl")
+    ccs_sub.ts8.synchCommand(10, "loadCategories RaftsLimits:itl")
+    logger.info("loaded configurations: Rafts:itl")
     try:
 # @todo: fix this
-#        logger.info("running powerOn %d command", rebid)
-#        result = ccs_sub.ts8.synchCommand(300, "powerOn %d" % rebid).getResult()
+#        command = "powerOn %d" % rebid
+#        logger.info("running %s", command)
+#        result = ccs_sub.ts8.synchCommand(300, command).getResult()
 #        logger.info(str(result))
         logger.info("------ %s Complete ------\n", rebname)
     except java.lang.Exception as eobj:
         logger.info(eobj.message)
-#        print "setting tick and monitoring period to 10s"
-#        ccs_sub.ts8.synchCommand(10, "change tickMillis 10000");
-#        raise eobj
+        raise eobj
+    finally:
+        print "Re-setting tick and monitoring period to 10s"
+        ccs_sub.ts8.synchCommand(10, "change monitor-update taskPeriodMillis 10000")
 
-#print "setting tick and monitoring period to 10s"
-#ccs_sub.ts8.synchCommand(10, "change tickMillis 10000");
-
-if current_check_ok:
+if power_on_ok:
     logger.info("DONE with successful powering of REBs.")
 else:
     logger.info("FAILED to turn on all requested REBs")
