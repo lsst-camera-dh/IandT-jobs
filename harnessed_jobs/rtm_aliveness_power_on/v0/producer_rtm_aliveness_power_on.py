175e30d90faa8b7608c20326b0767085bd256a45
Jim Chiang <jchiang@slac.stanford.edu>
2017-03-29 21:54:11 -0700

    remove directory permission workaround

--- harnessed_jobs/rtm_aliveness_power_on/v0/producer_rtm_aliveness_power_on.py
+++ harnessed_jobs/rtm_aliveness_power_on/v0/producer_rtm_aliveness_power_on.py
@@ -1,8 +1,3 @@
 #!/usr/bin/env python
 from ccsTools import ccsProducer
-import subprocess
-
-subprocess.check_call('chmod -R o+w /home/jchiang/work/jh_stage', shell=True)
-subprocess.check_call('chmod -R o+w /home/jchiang/work/jh_archive', shell=True)
-
 ccsProducer('rtm_aliveness_power_on', 'ccs_rtm_aliveness_power_on.py')
