e285a67af1c24e566226bcb48123067d11cfefdf
Jim Chiang <jchiang@slac.stanford.edu>
2017-05-19 11:39:48 -0700

    create png plots of pd current time histories

--- harnessed_jobs/ts8_preflight_acq/v0/producer_ts8_preflight_acq.py
+++ harnessed_jobs/ts8_preflight_acq/v0/producer_ts8_preflight_acq.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from ccsTools import ccsProducer, CcsRaftSetup
+
+ccsProducer('ts8_preflight_acq', 'ccs_ts8_preflight_acq.py',
+            ccs_setup_class=CcsRaftSetup)
+
+# Create png files of photodiode current vs time.
+pd_files = glob.glob('pd-values*.txt')
+for pd_file in pd_files:
+    pd_results = np.recfromtxt(pd_file, names=['time', 'current'])
+    plt.plot(pd_results.time*1e-9, pd_results.current)
+    plt.ylabel('Current (nA)')
+    plt.xlabel('Time (s)')   #TODO: check time units.
+    plt.savefig(pd_file.replace('.txt', '.png'))
--- harnessed_jobs/ts8_preflight_acq/v0/validator_ts8_preflight_acq.py
+++ harnessed_jobs/ts8_preflight_acq/v0/validator_ts8_preflight_acq.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+import glob
 import lcatr.schema
 
+fits_files = glob.glob('*.fits')
+txt_files = glob.glob('pd-values*.txt'
+png_files = glob.glob('pd-values*.png')
+
 results = []
+
 
 lcatr.schema.write_file(results)
 lcatr.schema.validate_file()
