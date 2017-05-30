578211e6e47b40cfa36c5a75278aa74e7df56f1a
Jim Chiang <jchiang@slac.stanford.edu>
2017-05-30 12:40:09 -0700

    implement ts8_ready_acq jython script

---
+++ harnessed_jobs/ts8_ready_acq/v0/ccs_ts8_ready_acq.py
@@ -0,0 +1,46 @@
+"""
+Jython script for readiness acquisitions at TS8.
+
+This is based on harnessed-jobs/T08/read_acq/ccseorebalive_exposure.py
+"""
+import time
+from eo_acquisition import EOAcquisition, PhotodiodeReadout, AcqMetadata, logger
+
+class ReadyAcquisition(EOAcquisition):
+    """
+    EOAcquisition subclass for "readiness" data.
+    """
+    def __init__(self, seqfile, acq_config_file, metadata, subsystems,
+                 logger=logger):
+        super(ReadyAcquisition, self).__init__(seqfile, acq_config_file,
+                                               "READY", metadata,
+                                               subsystems, logger=logger)
+        self.sub.pd.synchCommand(10, "setCurrentRange", 0.00002)
+
+    def run(self):
+        """
+        Take 3 images: 100ms Fe55, 1s flat, 4s flat.
+        """
+        # Fe55
+        seqno = 0
+        exptime = 0.1
+        openShutter = False
+        actuateXed = True
+        image_type = "FE55"
+        self.image_clears()
+        self.take_image(seqno, exptime, openShutter, actuateXed, image_type)
+
+        # Flats
+        openShutter = True
+        actuateXed = False
+        image_type = "FLAT"
+        for seqno, exptime in zip((1, 2), (1., 4.)):
+            self.image_clears()
+            self.take_image(seqno, exptime, openShutter, actuateXed, image_type)
+
+if __name__ == '__main__':
+    metadata = AcqMetadata(cwd=tsCWD, raft_id=UNITID, run_number=RUNNUM)
+    acq = ReadyAcquisition(sequence_file, rtmacqcfgfile, metadata, subsystems)
+
+    acq.run()
+    acq.sub.mono.synchCommand(900, "openShutter")
--- harnessed_jobs/ts8_ready_acq/v0/producer_ts8_ready_acq.py
+++ harnessed_jobs/ts8_ready_acq/v0/producer_ts8_ready_acq.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from ccsTools import ccsProducer, CcsRaftSetup
+
+ccsProducer('ts8_ready_acq', 'ccs_ts8_ready_acq.py',
+            ccs_setup_class=CcsRaftSetup,
+            sys_paths=(os.path.join(os.environ['IANDTJOBSDIR'], 'python'),))
