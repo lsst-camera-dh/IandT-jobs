e0e25f57fa5ef06c42637ccd71969bc8f10462b7
Jim Chiang <jchiang@slac.stanford.edu>
2017-06-06 18:00:07 -0700

    persist REB info to eT tables

--- python/eo_acquisition.py
+++ python/eo_acquisition.py
@@ -8,7 +8,7 @@ import time
 from collections import namedtuple
 import logging
 from ccs_scripting_tools import CcsSubsystems, CCS
-from ts8_utils import set_ccd_info
+from ts8_utils import set_ccd_info, write_REB_info
 
 __all__ = ["hit_target_pressure", "EOAcquisition", "PhotodiodeReadout",
            "EOAcqConfig", "AcqMetadata", "logger"]
@@ -111,6 +111,7 @@ class EOAcquisition(object):
                               mono='ts8/Monochromator')
         self.sub = CcsSubsystems(subsystems=subsystems, logger=logger)
         self.sub.write_versions(os.path.join(metadata.cwd, 'ccs_versions.txt'))
+        write_REB_info(self.sub.ts8)
         self._check_subsystems()
         set_ccd_info(self.sub, ccd_names, logger)
         self.sub.ts8.synchCommand(10, 'setDefaultImageDirectory %s/S${sensorLoc}'
