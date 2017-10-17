8972bb0407a534d2c48fdaaee957179aa8eb4621
James Chiang <jchiang@slac.stanford.edu>
2017-10-17 09:35:58 -0700

    changes to conform to LCA-10064-A

--- harnessed_jobs/rtm_aliveness_power_on/v0/ccs_rtm_aliveness_power_on.py
+++ harnessed_jobs/rtm_aliveness_power_on/v0/ccs_rtm_aliveness_power_on.py
@@ -10,6 +10,7 @@ import logging
 import java.lang
 from org.lsst.ccs.scripting import CCS
 from ccs_scripting_tools import CcsSubsystems
+from rebCurrentLimits import RebCurrentLimits
 
 CCS.setThrowExceptions(True)
 
@@ -18,9 +19,6 @@ logging.basicConfig(format="%(message)s",
                     stream=sys.stdout)
 logger = logging.getLogger()
 
-ChannelInfo = namedtuple('ChannelInfo', ['reb_ps_channel', 'ts8_mon_chan',
-                                         'low_lim', 'high_lim', 'chkreb'])
-
 def set_monitoring_interval(ts8, period_ms):
     "Set the CCS ts8 monitoring interval."
     command = "change monitor-update taskPeriodMillis %i" % period_ms
@@ -28,62 +26,83 @@ def set_monitoring_interval(ts8, period_ms):
     command = "change monitor-publish taskPeriodMillis %i" % period_ms
     ts8.synchCommand(10, command)
 
-def power_off_rebs(rebps, lines=(0, 1, 2)):
-    "Power-off the requested REBs via the specified power-lines."
-    for power_line in lines:
-        rebps.synchCommand(10, 'sequencePower', power_line, False)
+def reb_power_on(ccs_sub, rebid, power_line, ccd_type):
+    logger = ccs_sub.rebps.logger
 
-def map_power_lines_to_rebs(ccs_sub, ntries=20, wait_between_tries=10,
-                            num_lines=3):
-    return {i: i for i in range(num_lines)}
+    reb_current_limits = RebCurrentLimits(ccs_sub.rebps, ccs_sub.ts8)
 
-def check_values(ccs_sub, rebid, name, rebps_channel, ts8_mon_chan, low_lim,
-                 high_lim, chkreb, logger=logger):
-    """
-    Check that power supply current (or voltage) levels are within the
-    specified range.
-    """
-    reb_channel_name = 'REB%d.%s' % (rebid, rebps_channel)
-    command = "readChannelValue %s" % reb_channel_name
-    cur_ps = ccs_sub.rebps.synchCommand(10, command).getResult()
-    logger.info("REB PS: %s = %s", reb_channel_name, cur_ps)
+    rebname = 'REB%d' % rebid
+    logger.info("*****************************************************")
+    logger.info("Starting power-on procedure for %s (power line %s)",
+                rebname, power_line)
+    logger.info("*****************************************************")
 
-    ts8_channel_name = 'R00.Reb%d.%s' % (rebid, ts8_mon_chan)
-    command = "readChannelValue %s" % ts8_channel_name
-    cur_reb = ccs_sub.ts8.synchCommand(10, command).getResult()
-    logger.info("TS8 Monitor: %s = %s", ts8_channel_name, cur_reb)
+    # Power on the REB using the power-up sequence.
+    ccs_sub.rebps.synchCommand(10, 'sequencePower', power_line, True)
 
-    if cur_ps < low_lim or cur_ps > high_lim:
-        ccs_sub.rebps.synchCommand(10, 'sequencePower', rebid, False)
-        stat = "%s: %s with value %f mA not within specified range %f mA to %f mA.  Power to this channel has been shut off." % (rebname, reb_channel_name, cur_ps, low_lim, high_lim)
-        raise java.lang.Exception(stat)
+    # Check that the power-supply currents are within the limits
+    # for each channel.
+    reb_current_limits.check_rebps_limits(rebid)
 
-    if abs(cur_ps) > 0.0 and chkreb:
-        if abs(cur_reb - cur_ps)/cur_ps > 0.10 and abs(cur_reb) > 0.5:
-            logger.warning("WARNING: %s value differs by >10%% from %s value.",
-                           reb_channel_name, ts8_channel_name)
-    logger.info("")
+    # Wait 15 seconds for the FPGA to boot, then check currents again.
+    time.sleep(15)
+    reb_current_limits.check_rebps_limits(rebid)
+
+    # TODO: Verify data link integrity (LCA-10064-A, p.17, step 5)
+
+    # TODO: Read and record REB 1-wire global unique ID (GUID),
+    # then verify it is in the correct slot with power and data
+    # cables correctly configured (LCA-10064-A, p.17, step 6)
+
+    # TODO: Read and record the firmware version ID, then verify it is
+    # the correct version (LCA-10064-A, p.17, step 7)
+
+    # Check that REB P/S currents match the REB currents from ts8
+    # within the comparative limits.
+    reb_current_limits.check_comparative_ranges(rebid)
+
+    logger.info("Turn on REB clock and rail voltages.")
+
+    # Load sensor-specific configurations.
+    if ccd_type == 'ITL':
+        ccs_sub.ts8.synchCommand(10, "loadCategories Rafts:itl")
+        ccs_sub.ts8.synchCommand(10, "loadCategories RaftsLimits:itl")
+    elif ccd_type == 'E2V':
+        ccs_sub.ts8.synchCommand(10, "loadCategories Rafts:e2v")
+        ccs_sub.ts8.synchCommand(10, "loadCategories RaftsLimits:e2v")
+    else:
+        raise RuntimeError("ccs_rtm_aliveness_power_on: Invalid ccd_type, "
+                           + ccd_type)
+
+    try:
+        outfile = '%s_REB%i_%s_powerOn_aliveness_test_output.txt' \
+                  % (UNITID, rebid, RUNNUM)
+        outfile = '/'.join((tsCWD, outfile))
+        logger.info('Writing powerOn output to %s', outfile)
+        with open(outfile, 'w') as output:
+            output.write(str(ccs_sub.ts8.synchCommand(900, 'powerOn',
+                                                      rebid).getResult()))
+        os.chmod(outfile, 0664)
+        logger.info("------ %s power-on complete ------\n", rebname)
+    except java.lang.Exception as eobj:
+        logger.info(eobj.message)
+        raise eobj
+    finally:
+        set_monitoring_interval(ccs_sub.ts8, 10000)
 
 if __name__ == '__main__':
-    # @todo: Read channels to test and their limits from a configuration file.
-    channel \
-        = dict(digital=ChannelInfo('digital.IaftLDO', 'DigI', 6., 800., False),
-               analog=ChannelInfo('analog.IaftLDO', 'AnaI', 6., 610., False),
-               clockhi=ChannelInfo('clockhi.IaftLDO', 'ClkHI', 6., 300., True),
-               clocklo=ChannelInfo('clocklo.IaftLDO', 'ClkLI', 6., 300., True),
-               od=ChannelInfo('OD.IaftLDO', 'ODI', 6., 190., True))
-
-    logger.info("start tstamp: %f", time.time())
+    logger.info("Start time: %f", time.time())
 
     if subsystems is None:
         subsystems = dict(ts8='ts8', rebps='ccs-rebps')
 
     ccs_sub = CcsSubsystems(subsystems=subsystems, logger=logger)
 
-    logger.info("Mapping power supply lines to REBs...")
-    power_lines = map_power_lines_to_rebs(ccs_sub)
+    # Assumed mapping of power supply lines to REBs.
+    num_lines = 3
+    power_lines = {i: i for i in range(num_lines)}
 
-    logger.info("will attempt to power on and check currents for")
+    logger.info("Will attempt to power on and check currents for")
     for rebid, power_line in power_lines.items():
         logger.info("  power line %d for REB ID %d", power_line, rebid)
 
@@ -91,65 +110,7 @@ if __name__ == '__main__':
     set_monitoring_interval(ccs_sub.ts8, 100)
     time.sleep(3)
 
-    # This is the order to power on the various channels.
-    named_lines = ('master', 'digital', 'analog', 'clockhi', 'clocklo',
-                   'heater', 'od')
-
-    power_on_ok = True
     for rebid, power_line in power_lines.items():
-        rebname = 'REB%d' % rebid
-        logger.info("*****************************************************")
-        logger.info("Starting power-on procedure for %s (power line %s)",
-                    rebname, power_line)
-        logger.info("*****************************************************")
-        ccs_sub.rebps.synchCommand(10, 'sequencePower', power_line, True)
-        for name in named_lines:
-            # Check the channel values:
-            try:
-                if name in channel:
-                    check_values(ccs_sub, rebid, name, *channel[name])
-            except java.lang.Exception as eobj:
-                logger.info("%s: current check failed for %s",
-                            rebname, name)
-                logger.info(eobj.message)
-                power_on_ok = False
-                break
+        reb_power_on(ccs_sub, rebid, power_line, ccd_type)
 
-            time.sleep(2)
-
-        logger.info("Turn on REB clock and rail voltages.")
-
-        # Load sensor-specific configurations.
-        if ccd_type == 'ITL':
-            ccs_sub.ts8.synchCommand(10, "loadCategories Rafts:itl")
-            ccs_sub.ts8.synchCommand(10, "loadCategories RaftsLimits:itl")
-        elif ccd_type == 'E2V':
-            ccs_sub.ts8.synchCommand(10, "loadCategories Rafts:e2v")
-            ccs_sub.ts8.synchCommand(10, "loadCategories RaftsLimits:e2v")
-        else:
-            raise RuntimeError("ccs_rtm_aliveness_power_on: Invalid ccd_type, "
-                               + ccd_type)
-
-        try:
-            command = "powerOn %d" % rebid
-            outfile = '%s_REB%i_%s_powerOn_aliveness_test_output.txt' \
-                      % (UNITID, rebid, RUNNUM)
-            outfile = '/'.join((tsCWD, outfile))
-            logger.info('writing powerOn output to %s', outfile)
-            with open(outfile, 'w') as output:
-                output.write(ccs_sub.ts8.synchCommand(900, command).getResult())
-            os.chmod(outfile, 0664)
-            logger.info("------ %s power-on complete ------\n", rebname)
-        except java.lang.Exception as eobj:
-            logger.info(eobj.message)
-            set_monitoring_interval(ccs_sub.ts8, 10000)
-            raise eobj
-
-    set_monitoring_interval(ccs_sub.ts8, 10000)
-
-    if power_on_ok:
-        logger.info("DONE with successful powering of REBs.")
-    else:
-        logger.info("FAILED to turn on all requested REBs")
-
-    logger.info("stop tstamp: %f", time.time())
+    logger.info("Stop time: %f", time.time())
