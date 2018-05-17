64c1219978dc693e1f40884e102a39456fd1294a
Jim Chiang <jchiang@slac.stanford.edu>
2018-05-16 21:00:09 -0700

    add needed imports; fix plotting

--- harnessed_jobs/rtm_aliveness_exposure/v0/producer_rtm_aliveness_exposure.py
+++ harnessed_jobs/rtm_aliveness_exposure/v0/producer_rtm_aliveness_exposure.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import os
 import glob
+import matplotlib.pyplot as plt
+import lsst.afw.math as afw_math
 from ccsTools import ccsProducer, CcsRaftSetup
-import siteUtils
 import lsst.eotest.sensor as sensorTest
 import lsst.eotest.raft as raftTest
+import siteUtils
 from correlated_noise import correlated_noise, raft_level_oscan_correlations
 import camera_components
 
@@ -15,29 +17,30 @@ ccsProducer('rtm_aliveness_exposure', 'ccs_rtm_aliveness_exposure.py',
 raft_id = siteUtils.getUnitId()
 run_number = siteUtils.getRunNumber()
 
-raft = camera_components.Raft.create_from_etrav(raft_id)
+raft = camera_components.Raft.create_from_etrav(raft_id, db_name='Dev')
 results_files = dict()
 bias_files = dict()
 bbox = None
 for slot, sensor_id in raft.items():
     # glob the bias files for each sensor.  Use the last one in the sequence.
-    bias_files[slot] = sorted(glob.glob('%s_conn_bias_*.fits'))[-1]
+    bias_files[slot] = sorted(glob.glob('%s/%s_conn_bias_*.fits'
+                                        % (slot, sensor_id)))[-2:]
 
-    ccd = MaskedCCD(bias_files[slot])
+    ccd = sensorTest.MaskedCCD(bias_files[slot][-1])
 
     if bbox is None:
         bbox = ccd.amp_geom.serial_overscan
         bbox.grow(-10)
 
-    results = sensorTest.EOTestResults()
+    outfile = '%s_eotest_results.fits' % sensor_id
+    results = sensorTest.EOTestResults(outfile)
     for amp in ccd:
         image = ccd[amp].getImage()
         oscan = image.Factory(image, bbox)
         read_noise \
             = afw_math.makeStatistics(oscan, afw_math.STDEVCLIP).getValue()
         results.add_seg_result(amp, 'READ_NOISE', read_noise)
-    outfile = '%s_eotest_results.fits' % sensor_id
-    results.write(outfile)
+    results.write()
     results_files[slot] = outfile
 
 spec_plots = raftTest.RaftSpecPlots(results_files)
@@ -46,12 +49,14 @@ spec_plots.make_plot('READ_NOISE', 'noise per pixel (ADU rms)',
 plt.savefig('{}_{}_read_noise.png'.format(raft_id, run_number))
 
 for slot, sensor_id in raft.items():
-    _, corr_fig, _ = correlated_noise([bias_files[slot]]*2, target=0,
+    _, corr_fig, _ = correlated_noise(bias_files[slot], target=0,
                                       make_plots=True,
                                       title='{} {} {}'.format(slot, sensor_id,
                                                               run_number))
+    plt.figure(corr_fig.number)
     plt.savefig('{}_{}_correlated_noise.png'.format(sensor_id, run_number))
 
 title = 'Overscan correlations, {}, Run {}'.format(raft_id, run_number)
+bias_files = {slot: x[0] for slot, x in bias_files.items()}
 raft_level_oscan_correlations(bias_files, title=title)
-plt.savefig('{}_{}_overscan_correlations.png'.format(raft_id, run_number)
+plt.savefig('{}_{}_overscan_correlations.png'.format(raft_id, run_number))
