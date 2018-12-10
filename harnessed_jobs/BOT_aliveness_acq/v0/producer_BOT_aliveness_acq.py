70c7569edd3a931829a590d4288dad2e3b35ea10
James Chiang <jchiang@slac.stanford.edu>
2018-12-10 15:21:24 -0800

    obtain the BOT aliveness acq config filename from the top-level acq.cfg file so that the file can be set a runtime

--- harnessed_jobs/BOT_aliveness_acq/v0/producer_BOT_aliveness_acq.py
+++ harnessed_jobs/BOT_aliveness_acq/v0/producer_BOT_aliveness_acq.py
@@ -5,8 +5,13 @@ Producer script for BOT aliveness test acquisitions.
 import os
 import subprocess
 
-cfg_file = os.path.join(os.environ.get('LCATR_CONFIG_DIR', '.'),
-                        'bot_aliveness_acq.cfg')
+# Find the top-level acquisition configuration file.
+with open(os.path.join(os.environ['LCATR_CONFIG_DIR'], 'acq.cfg'), 'r') as fd:
+    for line in fd:
+        if line.startswith('bot_aliveness_acq_cfg'):
+            cfg_file = line.strip().split('=')[1].strip()
+            break
+
 run_number = os.environ['LCATR_RUN_NUMBER']
 
 command = '/home/ccs/bot-data.py --symlink . --run {} {}'.format(run_number,
