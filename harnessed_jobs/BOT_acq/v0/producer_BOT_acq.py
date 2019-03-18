2a7ac6d61f891b9b8a3643877460b4ffc3869f0f
James Chiang <jchiang@slac.stanford.edu>
2019-03-18 13:21:29 -0700

    create empty PRESERVE_SYMLINKS file

--- harnessed_jobs/BOT_acq/v0/producer_BOT_acq.py
+++ harnessed_jobs/BOT_acq/v0/producer_BOT_acq.py
@@ -7,6 +7,7 @@ passing that script the appropriate configuration file for the
 acquisition.
 """
 import subprocess
+import pathlib
 import siteUtils
 
 run_number = siteUtils.getRunNumber()
@@ -17,3 +18,4 @@ command = '/home/ccs/bot-data.py --symlink . --run {} {}'\
     .format(run_number, acq_config['bot_eo_acq_cfg'])
 
 subprocess.check_call(command, shell=True)
+pathlib.Path('PRESERVE_SYMLINKS').touch()
