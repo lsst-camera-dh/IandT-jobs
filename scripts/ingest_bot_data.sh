6934544b960ce5bfca6f095329e8afcbe1dc3d9b
James Chiang <jchiang87@gmail.com>
2021-09-27 15:39:28 -0700

    Use BOT_data repo for ingest; add HMS to timestamp for log file; srun in bg

--- scripts/ingest_bot_data.sh
+++ scripts/ingest_bot_data.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-repo=/sdf/group/lsst/camera/IandT/repo_gen3/bot_data
+repo=/sdf/group/lsst/camera/IandT/repo_gen3/BOT_data
 bot_data_folder=$1
 
-weekly=w_2021_38
+weekly=w_2021_39
 source /cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib/${weekly}/loadLSST.bash
 setup lsst_distrib
 export OMP_NUM_THREADS=1
-timestamp=`date +%Y-%m-%d`
+timestamp=`date +%Y-%m-%d_%H.%M.%S`
 srun --time=02:00:00 --output=${script_dir}/bot_ingest_${timestamp}.log \
-     python ${script_dir}/ingest_bot_data.py --repo ${repo} ${bot_data_folder}
+     python ${script_dir}/ingest_bot_data.py --repo ${repo} ${bot_data_folder} &
