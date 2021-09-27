8e3e4447d560127367227513164243edf64f8baf
James Chiang <jchiang87@gmail.com>
2021-09-27 15:38:39 -0700

    use logger instead of print; keep track of frames that are missing required keywords

--- scripts/ingest_bot_data.py
+++ scripts/ingest_bot_data.py
@@ -3,10 +3,12 @@ Script to ingest BOT data from a folder containing subfolders for
 each focal plane frame.
 """
 import os
+import sys
 import glob
 import json
 import subprocess
 import argparse
+import logging
 
 parser = argparse.ArgumentParser()
 parser.add_argument('bot_data_folder', type=str,
@@ -18,12 +20,17 @@ parser.add_argument('--frame_prefix', type=str, default='MC_C_',
 
 args = parser.parse_args()
 
+logging.basicConfig(format='%(asctime)s %(name)s: %(message)s',
+                    stream=sys.stdout)
+logger = logging.getLogger('ingest_bot_data.py')
+logger.setLevel(logging.INFO)
+
 # Make a list of non-empty frame subfolders
 pattern = os.path.join(args.bot_data_folder, f'{args.frame_prefix}*')
 frame_folders = [_ for _ in sorted(glob.glob(pattern)) if
                  glob.glob(os.path.join(_, '*.fits'))]
 
-index_name = '_index.json'
+INDEX_NAME = '_index.json'
 # Add the index files to each folder.
 access_restricted = []
 for folder in frame_folders:
@@ -32,7 +39,7 @@ for folder in frame_folders:
         # add to list for later reporting.
         access_restricted.append(folder)
         continue
-    if os.path.isfile(os.path.join(folder, index_name)):
+    if os.path.isfile(os.path.join(folder, INDEX_NAME)):
         # Skip if index file already exists.
         continue
 #    command = ('astrometadata -p lsst.obs.lsst.translators write-index '
@@ -42,9 +49,10 @@ for folder in frame_folders:
     subprocess.check_call(command, shell=True)
 
 # Run butler ingest-raws on each folder.
+missing_keywords = []
 required_keywords = ['EXPTIME', 'RUNNUM']
 for folder in frame_folders:
-    index_file = os.path.join(folder, index_name)
+    index_file = os.path.join(folder, INDEX_NAME)
     if not os.path.isfile(index_file):
         # Skip ingest if index file is missing.
         continue
@@ -53,11 +61,18 @@ for folder in frame_folders:
         indexes = json.load(fd)
     if ('__COMMON__' not in indexes or
         any(_ not in indexes['__COMMON__'] for _ in required_keywords)):
+        missing_keywords.append(folder)
         continue
+    logger.info('ingesting %s', folder)
     command = (f'butler ingest-raws --transfer=direct {args.repo} {folder}')
     subprocess.check_call(command, shell=True)
 
 if access_restricted:
-    print('Access restricted folders:')
+    logger.info('Access restricted folders:')
     for folder in access_restricted:
-        print(' ', folder)
+        logger.info(' %s', folder)
+
+if missing_keywords:
+    logger.info('Frames with missing required keywords:')
+    for folder in missing_keywords:
+        logger.info(' %s', folder)
