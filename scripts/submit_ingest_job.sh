a079b92bfc2d8088f0c21a678027eb94b04b8674
James Chiang <jchiang87@gmail.com>
2021-12-01 13:38:39 -0800

    switch from srun to sbatch

---
+++ scripts/submit_ingest_job.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+bot_data_folder=$1
+if [[ $# == 2 ]];
+then
+    repo=$2
+else
+    repo=/sdf/group/lsst/camera/IandT/repo_gen3/BOT_data
+fi
+
+export SCRIPT_DIR=${script_dir}
+export REPO=${repo}
+export BOT_DATA_FOLDER=${bot_data_folder}
+
+timestamp=`date +%Y-%m-%d_%H.%M.%S`
+log_file=${script_dir}/bot_ingest_${timestamp}.log
+job_name=ingest-bot-${bot_data_folder}
+
+sbatch --export=ALL --output=${log_file} --job-name=${job_name} \
+    ${script_dir}/ingest_bot_data.sbatch
